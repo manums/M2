c4b33d6d57e0d010e796b23e5b9b8b5711191d3d
Manu Shivakumar <Manu Shivakumar>
2017-01-16 16:04:21 +0530

    Adding Submodule to S1 and also using it

--- M2-TestS1/M2-TestS1/Program.cs
+++ M2-TestS1/M2-TestS1/Program.cs
@@ -10,6 +10,11 @@ namespace M2_TestS1
     {
         static void Main(string[] args)
         {
+            SharedLogging.Logger logger = new SharedLogging.Logger();
+            logger.WriteToFile("D:\\test.log", "I'm testing local logging from Solution M1");
+
+            Helper.SharedHelper helper = new Helper.SharedHelper();
+            Console.WriteLine(helper.GetLength("hello"));
         }
     }
 }
